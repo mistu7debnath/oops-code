d0e18208c2f3e9710921fed818e6ef205c46e5ab
Manisha <mistu7debnath2021@gmail.com>
2026-01-05 22:35:47 +0530

    message

---
+++ InheritanceExample.java
@@ -0,0 +1,26 @@
+// Parent class
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+// Child class (inherits Animal)
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+// Main class
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        // Calling inherited method
+        d.eat();
+
+        // Calling child class method
+        d.bark();
+    }
+}
