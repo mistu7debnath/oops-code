116a38bbb07735aa139d17507011532e57f1ed11
Manisha <mistu7debnath2021@gmail.com>
2026-01-05 23:22:17 +0530

    message

---
+++ PolymorphismExample.java
@@ -0,0 +1,18 @@
+class Shape {
+    void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+public class PolymorphismExample {
+    public static void main(String[] args) {
+        Shape s = new Circle();   // upcasting
+        s.draw();
+    }
+}
